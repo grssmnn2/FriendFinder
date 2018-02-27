576d56e36ae7b95ea5c65a397a06fd31cdad8f4c
Cecily Grossmann <grssmnn2@gmail.com>
2018-02-27 16:12:44 -0600

    added pseudocode

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,33 +8,30 @@ app.get("/api/friends", function(req, res) {
     res.json(friends);
 });
 
-// send data from input to friends.js to be stored as JSON
-// store information at this route as users finish survey
+// add information to the /api/friends route as users finish survey
 app.post("/api/friends", function(req, res){
     // req.body is equal to the entire JSON post sent from the user
     var newUser = req.body; 
-  
+//   loop through the entire friends list
     for (var i = 0; i < friends.length; i++) {  
         var difference;
         var maxDiff= 1000;
         var bestMatch;
         var bestMatchPhoto;
-        // create a variable difference starting at zero
-        // within this loop through each user's score array
+        // within this loop through each friends' score array
         for (var j=0; j<newUser.scores[j].length; i++){
-            //find absolute value of the difference between user score and stored friend score 
+            //find absolute value of the difference between user score and friend score 
             difference = Math.abs(newUser.scores[j]-friends[i].scores[j]);
         }
-        // if this value is the new smallest difference, put this in variable maxDiff
-        if(difference < maxDiff){
+        // if this value is the new smallest difference,
+        if(difference < maxDiff){  
+            // put this in variable maxDiff
             maxDiff = difference;
+            // store that specific friend match in the bestMatch variable
             bestMatch = friends[i].name;
             bestMatchPhoto = friends[i].photo;
         }
-        // send whatever sits in the smallest difference variable to the modal once loop is done
-
     }
-    // send that user's photo and name to the modal div on survey.html
     // add new user to the friends array
     friends.push(newUser);
 });
