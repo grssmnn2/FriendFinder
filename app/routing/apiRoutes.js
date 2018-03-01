84ac6200064bc6cdabc0460c3eb4241090329340
Cecily Grossmann <grssmnn2@gmail.com>
2018-02-28 22:16:02 -0600

    trying to make loops work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,22 +11,20 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
     // req.body is equal to the entire JSON post sent from the user
     var newUser = req.body;
-    console.log(newUser);
-    //   loop through the entire friends list
-    var difference = 0;
     var minDiff = 500;
     var bestMatch;
     var bestMatchPhoto;
-    for (var i = 0; i < friends.length; i++) {
+     //   loop through the entire friends list    
+      for (var i = 0; i < res.length; i++) {
       // within this loop through each friends' score array
-      for (var j = 0; j < friends[i].scores.length; i++) {
+      var difference = 0;
+      for (var j = 0; j < res[i].scores[j].length; j++) {
         //find absolute value of the difference between user score total and friend score total
-        var reducedFriend = friends[i].scores.reduce(function(total, nextInput) {
-          return total + nextInput;
-        });
-        var reducedUser = newUser.scores.reduce(function(total, nextInput) {
-          return total + nextInput;
-        });
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        var reducedFriend = res[i].scores[j];
+        var reducedUser = newUser.scores[j];
+        reducedFriend.reduce(reducer);
+        reducedUser.reduce(reducer);
         difference = Math.abs(reducedUser - reducedFriend);
     }
         // if this value is the new smallest difference,
@@ -34,11 +32,14 @@ module.exports = function(app) {
           // put this in variable minDiff
           minDiff = difference;
           // store that specific friend match in the bestMatch variable
-          bestMatch = friends[i].name;
-          bestMatchPhoto = friends[i].photo;
+          bestMatch = res[i].name;
+          bestMatchPhoto = res[i].photo;
       }
     }
     // add new user to the friends array AFTER finding match so user doesn't match with themselves
     friends.push(newUser);
+
+    // send best match name + photo to modal on survey page
+
   });
 };
