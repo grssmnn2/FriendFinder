5c7f568d08b9dd910a2927fe2d7f257e54382322
Cecily Grossmann <grssmnn2@gmail.com>
2018-03-02 18:49:55 -0600

    img and name are popping up in modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,7 +7,7 @@ var friends = [
 {
     "name": "Barack",
     "photo": "https://www.biography.com/.image/t_share/MTQ1NDY1MzcwMzY5NTk5MDMx/barack-obama---americas-first-african-american-president.jpg",
-    "scores[]": [
+    "scores": [
     "1",
     "1",
     "2",
@@ -23,7 +23,7 @@ var friends = [
     {
     "name": "Fred",
     "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6UcjS9buXf7s2KB76oHMQLXu_OTz3o1tz3Uf3Q-HHi3S28TF1Q",
-    "scores[]": [
+    "scores": [
     "1",
     "1",
     "2",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,37 +9,37 @@ module.exports = function(app) {
 
   // add information to the /api/friends route as users finish survey
   app.post("/api/friends", function(req, res) {
+    console.log(friends.length);
     // req.body is equal to the entire JSON post sent from the user
     var newUser = req.body;
     var minDiff = 500;
     var bestMatch;
     var bestMatchPhoto;
+    var difference = 0;
      //   loop through the entire friends list    
-      for (var i = 0; i < res.length; i++) {
-      // within this loop through each friends' score array
-      var difference = 0;
-      for (var j = 0; j < res[i].scores[j].length; j++) {
+      for (var i = 0; i < friends.length; i++) {
+      // within this loop through each friends' score array     
+      for (var j = 0; j < friends[i].scores.length; j++) {
         //find absolute value of the difference between user score total and friend score total
-        const reducer = (accumulator, currentValue) => accumulator + currentValue;
-        var reducedFriend = res[i].scores[j];
-        var reducedUser = newUser.scores[j];
-        reducedFriend.reduce(reducer);
-        reducedUser.reduce(reducer);
-        difference = Math.abs(reducedUser - reducedFriend);
+        var friendScores = friends[i].scores[j];
+        var userScores = newUser.scores[j];
+        difference += Math.abs(userScores - friendScores);
     }
-        // if this value is the new smallest difference,
+   
+        // if this value is the new smallest difference,    
         if (difference < minDiff) {
           // put this in variable minDiff
           minDiff = difference;
           // store that specific friend match in the bestMatch variable
-          bestMatch = res[i].name;
-          bestMatchPhoto = res[i].photo;
+          bestMatch = friends[i].name;
+          bestMatchPhoto = friends[i].photo;
       }
+ 
     }
     // add new user to the friends array AFTER finding match so user doesn't match with themselves
     friends.push(newUser);
 
-    // send best match name + photo to modal on survey page
-
+    // send best match name + photo to modal on survey page using object and
+    res.json({"bestMatch": bestMatch, "bestMatchPhoto": bestMatchPhoto});
   });
 };
